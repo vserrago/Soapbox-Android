e08fea3d88fb669db5420ed842e10afa9f5ae344
Valentin Serrago <ogarres@gmail.com>
2013-03-25 21:20:01 -0400

    Add DisplayShoutListTask

---
+++ src/com/example/soapbox/DisplayShoutListTask.java
@@ -0,0 +1,18 @@
+package com.example.soapbox;
+
+import org.json.JSONObject;
+
+import android.os.AsyncTask;
+
+public class DisplayShoutListTask extends AsyncTask<String, String, JSONObject>
+{
+
+
+	@Override
+	protected JSONObject doInBackground(String... params) 
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
